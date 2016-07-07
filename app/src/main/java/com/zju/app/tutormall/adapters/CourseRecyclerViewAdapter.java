d88c264d46c5fdcd989ab4dba4a23ed711b22e18
vkazas <vkazas@163.com>
2016-07-07 15:41:57 +0800

    finished with sliders ready to add search view

--- app/src/main/java/com/zju/app/tutormall/adapters/CourseRecyclerViewAdapter.java
+++ app/src/main/java/com/zju/app/tutormall/adapters/CourseRecyclerViewAdapter.java
@@ -13,10 +13,10 @@ import com.zju.app.tutormall.R;
 
 public class CourseRecyclerViewAdapter extends RecyclerView.Adapter<CourseRecyclerViewAdapter.CourseViewHolder> {
 
-    private static String[] courseInfo = {"微积分", "大学物理"};
-    private static String[] teacherInfo = {"陈铭业 浙江大学物理学教授", "陈铭业 浙江大学车队队长"};
-    private static String[] scheduleInfo = {"周二上课" , "周四上午上课 紫金港"};
-    int itemCnt = 2;
+    private static String[] courseInfo = {"微积分", "大学物理", "食用医学", "形势与政策2", "权力的游戏", "如何食用人类", "宇宙起源", "Riptide"};
+    private static String[] teacherInfo = {"陈铭业 浙江大学物理学教授", "陈铭业 浙江大学车队队长", "陈铭业 浙江大学后勤队长", "陈铭业 浙江大学保卫处长", "陈铭业 浙江大学校长", "陈铭业 浙江大学拉拉队队长", "陈铭业 浙江大学主厨", "陈铭业 浙江大学发言人"};
+    private static String[] scheduleInfo = {"周二上课" , "周四上午上课 紫金港", "周四上午上课 之江", "周四上午上课 玉泉", "周四上午上课 钓鱼台", "周四上午上课 维斯特洛", "周四上午上课 洛宁", "周四上午上课 游泳池"};
+    int itemCnt = 8;
 
     public class CourseViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
--- app/src/main/java/com/zju/app/tutormall/fragments/PropagateFragment.java
+++ app/src/main/java/com/zju/app/tutormall/fragments/PropagateFragment.java
@@ -1,18 +1,27 @@
 package com.zju.app.tutormall.fragments;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.zju.app.tutormall.R;
+import com.zju.app.tutormall.adapters.CourseRecyclerViewAdapter;
+import com.zju.app.tutormall.adapters.RecyclerViewDivider;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+
+
 public class PropagateFragment extends Fragment {
+
+    private int maxSlider = 4;
 
 
     public PropagateFragment() {
@@ -23,9 +32,42 @@ public class PropagateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_propagate, container, false);
+        View view = inflater.inflate(R.layout.fragment_propagate, container, false);
+
+        /**
+         * Setting up sliders
+         * */
+        SliderLayout sliderShow = (SliderLayout) view.findViewById(R.id.propagate_slider);
+        ArrayList<TextSliderView> sliders = new ArrayList<TextSliderView>();
+        int[] images = {R.drawable.bigbang, R.drawable.game_of_thrones, R.drawable.hannibal, R.drawable.house};
+        String[] descriptions = {"Bigbang", "Game of Thrones", "Hannibal", "House of Cards"};
+        for (int i=0; i<maxSlider; i++) {
+            TextSliderView slide = new TextSliderView(getActivity());
+            slide.description(descriptions[i]).image(images[i]);
+            sliders.add(slide);
+            sliderShow.addSlider(slide);
+        }
+        sliderShow.setPresetTransformer("CubeIn");
+
+        /**
+         * Setting up recycler list
+         * */
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.propagate_course_list);
+        setupRecyclerView(rv);
+
+
+        return view;
     }
 
+    private void setupRecyclerView(RecyclerView rv) {
+        rv.setLayoutManager(new LinearLayoutManager(rv.getContext()));
+        rv.setAdapter(new CourseRecyclerViewAdapter(getActivity()));
+
+        // divider between items
+        rv.addItemDecoration(new RecyclerViewDivider(getActivity(),
+                LinearLayoutManager.VERTICAL,
+                20,
+                getResources().getColor(R.color.mistyrose)));
+    }
 
 }
